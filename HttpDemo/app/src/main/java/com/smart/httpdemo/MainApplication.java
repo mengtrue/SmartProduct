1922fc622847baee31fbe5deb9a4dfd9c8e5ebc6
mengtrue <jmc313@163.com>
2018-07-03 18:08:47 +0800

    add Material login activity

---
+++ HttpDemo/app/src/main/java/com/smart/httpdemo/MainApplication.java
@@ -0,0 +1,71 @@
+package com.smart.httpdemo;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.StringRes;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.Utils;
+
+public class MainApplication extends Application {
+    private static final String TAG = MainApplication.class.getSimpleName();
+    /**
+     * the running activity number
+     * if the count is zero, the APP not run
+     */
+    private static int mActivityCount = 0;
+
+    private LogUtils.Config mLogConfig = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Utils.init(this);
+        this.registerActivityLifecycleCallbacks(new SmartActivityLifeCycleCallback());
+        mLogConfig = LogUtils.getConfig();
+        LogUtils.dTag(TAG, mLogConfig);
+    }
+
+    /**
+     * get resource string from resId
+     * @param resId
+     * @return the resource string
+     */
+    public String getResString(@StringRes int resId) {
+        Context context = getApplicationContext();
+        return (context == null) ? null : context.getResources().getString(resId);
+    }
+
+    public int getRunningActivityCount() {
+        return mActivityCount;
+    }
+
+    private class SmartActivityLifeCycleCallback implements ActivityLifecycleCallbacks {
+        @Override
+        public void onActivityCreated(Activity activity, Bundle bundle) {
+            mActivityCount++;
+        }
+
+        @Override
+        public void onActivityDestroyed(Activity activity) {
+            mActivityCount--;
+        }
+
+        @Override
+        public void onActivityResumed(Activity activity) {}
+
+        @Override
+        public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {}
+
+        @Override
+        public void onActivityStopped(Activity activity) {}
+
+        @Override
+        public void onActivityStarted(Activity activity) {}
+
+        @Override
+        public void onActivityPaused(Activity activity) {}
+    }
+}
---
+++ HttpDemo/app/src/main/java/com/smart/httpdemo/activity/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.smart.httpdemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.smart.httpdemo.R;
+
+public class LoginActivity extends BaseActivity {
+
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btSignin;
+    private CardView cardView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void initView() {
+        etUsername = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        btSignin = findViewById(R.id.bt_enter);
+        cardView = findViewById(R.id.cardView);
+    }
+}
